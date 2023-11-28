951456fa4087eaa2e9438e48680b1c6cb15c2ae9
yeonh0 <nanyeonho@gmail.com>
2023-11-28 19:05:47 +0900

    시리얼 스레드, 소켓 스레드 만들어서 시리얼 스레드는 값 받아오고 큐에 쌓고 소켓 스레드는 큐에서 값 받아와서 socket io에 집어넣음

--- flask/app.py
+++ flask/app.py
@@ -1,32 +1,57 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-from serial import Serial
+import serial
+import threading
+from queue import Queue
+import time
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-
-ser = Serial('COM5', 9600)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-# 시리얼 통신 스레드 만들어서 데이터 받음 -> 큐에 저장
-# 소켓 IO에서는 큐에 저장된 값을 pop해서 값 가져와서 socket io로 보내기
-# 큐 사이즈 이상으로 데이터 보내면 예외처리, 데이터 없을 때 뺴는 경우 예외처리
+# 시리얼 포트 설정 (COM5, 9600 bps)
+ser = serial.Serial('COM5', 9600)
 
-def background_thread():
+# 큐 생성
+serial_queue = Queue(maxsize=10)  # 큐 사이즈 설정
+
+# 시리얼 통신을 처리하는 스레드 함수
+def serial_thread():
     while True:
-        if ser.readable():              # sleep -> while문 줄임,  socet io로 전송만
-            res = ser.readline()
-            socketio.emit('serial_data', {'data': res.decode()[:len(res)-1]})
-            print(res.decode()[:len(res)-1])
+        try:
+            print(ser.in_waiting)
+            if ser.in_waiting > 0:      # 시리얼 포트 버퍼에 읽을 수 있는 바이트 > 0 -> 데이터를 읽어옴
+                received_data = ser.readline().decode('utf-8').strip()
+                serial_queue.put(received_data, block=False)  # 큐에 저장, block=False로 설정하여 예외 발생
+        except Exception as e:
+            print(f"Serial Thread Exception: {e}")
+        time.sleep(0.1)  # 작업 간격을 조절하여 무한 루프에서 너무 빠르게 동작하지 않도록 함
+
+# 소켓 통신을 처리하는 백그라운드 스레드 함수
+def socket_thread():
+    while True:
+        try:
+            if not serial_queue.empty():        # 큐가 비어있지 않을 경우zㅋ
+                data = serial_queue.get(block=False)  # 큐에서 데이터 가져오기, block=False로 설정하여 예외 발생
+                socketio.emit('serial_data', {'data': data})
+                print(f"Sent to socket: {data}")
+        except Exception as e:
+            print(f"Socket Thread Exception: {e}")
+        time.sleep(0.1)  # 작업 간격을 조절하여 무한 루프에서 너무 빠르게 동작하지 않도록 함
+
+# 스레드 생성 및 시작
+serial_thread_obj = threading.Thread(target=serial_thread)
+socket_thread_obj = threading.Thread(target=socket_thread)
+
 
 @socketio.on('connect')                 # 큐에 저장된 값을 빼서
 def handle_connect():
     print('Client connected')
-    socketio.start_background_task(target=background_thread)
-
+    socketio.start_background_task(target=serial_thread)
+    socketio.start_background_task(target=socket_thread)
 
 
 if __name__ == '__main__':
